8aa22416ed239c48bc7ca7fc2ded800fbce30a8f
DonizeteVida <donizetevida1998@outlook.com>
2026-05-31 00:08:07 +0200

    Add basic player movement

--- Player.cs
+++ Player.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Player : Area2D
 {
+	[Export]
+	public int Speed { get; set; } = 400;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +14,43 @@ public partial class Player : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	override public void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Input.GetVector(
+			"ui_left",
+			"ui_right",
+			"ui_up",
+			"ui_down"
+		);
+
+		var viewport = GetViewportRect().Size;
+
+		Position += velocity * Speed * (float)delta;
+		Position = Position.Clamp(Vector2.Zero, viewport);
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity != Vector2.Zero)
+		{
+			animatedSprite2D.FlipH = velocity.X < 0;
+			animatedSprite2D.FlipV = velocity.Y > 0;
+
+			if (velocity.Y != 0)
+			{
+				animatedSprite2D.Animation = "up";
+			}
+			else if (velocity.X != 0)
+			{
+				animatedSprite2D.Animation = "walk";
+			}
+
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
 	}
 }
