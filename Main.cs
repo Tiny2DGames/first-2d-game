040f3af4748d90b62fe0493868589fac26e4a9fd
DonizeteVida <donizetevida1998@outlook.com>
2026-05-31 22:13:10 +0200

    Add score counting timer

--- Main.cs
+++ Main.cs
@@ -6,6 +6,8 @@ public partial class Main : Node
 	[Export]
 	public PackedScene MobScene { get; set; }
 
+	private uint _score = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -32,12 +34,18 @@ public partial class Main : Node
 
 		var direction = player.Position - mobSpawnLocation.Position;
 		var angle = direction.Angle();
-		var velocity = new Vector2(GD.RandRange(25, 400), 0).Rotated(angle);
+		var velocity = new Vector2(GD.RandRange(75, 400), 0).Rotated(angle);
 
 		mob.Rotation = angle;
 		mob.LinearVelocity = velocity;
 		mob.Position = mobSpawnLocation.Position;
 
 		AddChild(mob);
+	}
+
+	private void OnScoreTimeout()
+	{
+		_score++;
+		GD.Print($"Score: {_score}");
 	}
 }
