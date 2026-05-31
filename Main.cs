5ac88ba41fbb1e2b953926224913f1b85b5dd67d
DonizeteVida <donizetevida1998@outlook.com>
2026-05-31 22:07:56 +0200

    Listen for Player's Hit callback

--- Main.cs
+++ Main.cs
@@ -9,11 +9,17 @@ public partial class Main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetNode<Player>("Player").Hit += OnPlayerHit;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void OnPlayerHit()
+	{
+		GetTree().ReloadCurrentScene();
 	}
 
 	private void OnMobTimerTimeout()
